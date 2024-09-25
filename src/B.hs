da6f6e0857a5b7e35a2d9877f94b065e979d9443
Vanessa McHale <vamchale@gmail.com>
2024-09-25 14:04:53 -0400

    plod

--- src/B.hs
+++ src/B.hs
@@ -4,19 +4,22 @@ import           A
 import           Control.Monad.State.Strict (StateT, runStateT)
 import           Data.Bifunctor             (second)
 import qualified Data.IntMap                as IM
+import           Nm
+import           Ty.Clone
 
-type Β=IM.IntMap Int
-data BSt=Bs { maxb :: !Int, bb :: Β }
+type Cs a=IM.IntMap ([Nm a], T a)
+type Β a=IM.IntMap (T a)
+data BSt a=Bs { maxb :: !Int, bb :: Β a }
 
 data BE a
 
-type BM a = StateT BSt (Either (BE a))
+type BM a = StateT (BSt a) (Either (BE a))
 
-β :: Int -> T a -> T a -> T a
-β = undefined
+β :: Int -> Cs a -> T a -> [T a] -> Either (BE a) (T a, Int)
+β i c t bs = runBM i (bM c t bs) -- undefined
 
 runBM :: Int -> BM a x -> Either (BE a) (x, Int)
 runBM i = fmap (second maxb).flip runStateT (Bs i IM.empty)
 
-bM :: T a -> T a -> BM a (T a)
+bM :: Cs a -> T a -> [T a] -> BM a (T a)
 bM = undefined
