50cde6fc201babfbe5454512cabc88df93cc9fda
Vanessa McHale <vamchale@gmail.com>
2024-09-25 23:12:58 -0400

    pattern-match exhaustiveness check

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,7 +4,7 @@ module Main (main) where
 
 import           P
 import           Test.Tasty       (TestTree, defaultMain, testGroup)
-import           Test.Tasty.HUnit (Assertion, assertFailure, testCase)
+import           Test.Tasty.HUnit (Assertion, assertFailure, testCase, (@?=))
 
 main :: IO ()
 main = defaultMain $
@@ -17,10 +17,18 @@ main = defaultMain $
       , rFile [] "examples/vierergruppe.piz"
       , tFile [] "prelude/fn.piz"
       , tFile ["."] "test/examples/ifte.piz"
+      , tErr ["."] "test/data/pmfail.piz" "8:17: Failed to match '`just' against 'a `just ⊕ `nil'"
       ]
 
 rFile :: [FilePath] -> FilePath -> TestTree
 rFile incls fp = testCase fp $ rr incls fp
+
+tErr :: [FilePath] -> FilePath -> String -> TestTree
+tErr incls fp expected = testCase fp $ do
+    res <- tMs incls fp
+    case res of
+        Right{} -> assertFailure "expected error."
+        Left e  -> show e @?= expected
 
 tFile :: [FilePath] -> FilePath -> TestTree
 tFile incls fp = testCase fp $ do
