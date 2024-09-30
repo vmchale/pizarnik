cbe77b04f8e35ab7ab82dea5c53efa371f4ce3fe
Vanessa McHale <vamchale@gmail.com>
2024-09-30 14:54:08 -0700

    Nm.Map

---
+++ src/Nm/Map.hs
@@ -0,0 +1,12 @@
+module Nm.Map ( insert
+              , fromList
+              ) where
+
+import qualified Data.IntMap as IM
+import           Nm
+
+fromList :: [(Nm a, b)] -> IM.IntMap b
+fromList xs = IM.fromList [ (i,x) | (Nm _ (U i) _, x) <- xs ]
+
+insert :: Nm a -> b -> IM.IntMap b -> IM.IntMap b
+insert (Nm _ (U i) _) x = IM.insert i x
