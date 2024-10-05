6e82d73e0f8f43735f601bc8acdd4e3198fdbe16
Vanessa McHale <vamchale@gmail.com>
2024-10-04 18:05:42 -0700

    spurious

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -4,11 +4,7 @@ import           Criterion.Main (bench, bgroup, defaultMain, whnfIO)
 import           P
 
 main = defaultMain
-    [ bgroup "r"
-        [ bench "test/deps/c.piz" $ whnfIO $ rMs ["test/deps"] "test/deps/c.piz"
-        , bench "lib/list.piz" $ whnfIO $ rMs ["."] "lib/list.piz"
-        ]
-    , bgroup "t"
+    [ bgroup "t"
         [ bench "test/examples/ifte.piz" $ whnfIO $ tMs ["."] "test/examples/ifte.piz"
         , bench "examples/vierergruppe.piz" $ whnfIO $ tMs ["."] "examples/vierergruppe.piz"
         ]
