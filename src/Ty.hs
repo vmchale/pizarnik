19bd466a09853a7f72ca2f9112b526e405d2a001
Vanessa McHale <vamchale@gmail.com>
2024-10-07 17:31:03 -0400

    I was being silly

--- src/Ty.hs
+++ src/Ty.hs
@@ -191,11 +191,10 @@ expr :: TS a -> TS a -> TM a (TS a, TS a)
 expr ts0@(TS l0 r0) ts1@(TS l1 r1) =
     case compare n0r n1r of
         EQ -> pure (ts0,ts1)
-        GT -> let a=n0r-n1r in do {ρ <- fρ (tLs l0) a; pure (ts0, (TS&:(ρ++)) l1 r1)}
-        LT -> let a=n1r-n0r in do {ρ <- fρ (tLs l1) a; pure ((TS&:(ρ++)) l0 r0, ts1)}
+        GT -> do {ᴅ <- fsv (tLs l0) "ᴅ"; pure (ts0, (TS&:(ᴅ:)) l1 r1)}
+        LT -> do {ᴅ <- fsv (tLs l1) "ᴅ"; pure ((TS&:(ᴅ:)) l0 r0, ts1)}
   where
     n0r=length r0; n1r=length r1
-    fρ x n = zipWithM (\_ c -> ftv x (T.singleton c)) [1..n] ['c'..]
 
 {-# SCC uac #-}
 uac :: F -> Subst a -> T a -> T a -> TM a (T a, Subst a)
