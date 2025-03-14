6ea31354f03c185e57ba7837881001fe89d50516
Vanessa McHale <vamchale@gmail.com>
2025-03-14 05:38:59 -0400

    done

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,25 +4,23 @@ module Main (main) where
 
 import           P
 import           Test.Tasty       (TestTree, defaultMain, testGroup)
-import           Test.Tasty.HUnit (Assertion, assertFailure, testCase, (@?=))
+import           Test.Tasty.HUnit (assertFailure, testCase, (@?=))
 
 main :: IO ()
 main = defaultMain $
     testGroup "unit tests"
-      [ tFile ["."] "lib/list.piz"
-      , tFile ["."] "prelude/ord.piz"
-      , tFile ["."] "test/examples/bool.piz"
-      , rFile ["."] "test/examples/mutual.piz"
-      , tFile ["."] "test/examples/maybe.piz"
-      , tFile ["."] "test/data/beta.piz"
-      , tFile [] "examples/vierergruppe.piz"
-      , tFile [] "prelude/fn.piz"
-      , tFile ["."] "test/examples/ifte.piz"
-      , tErr ["."] "test/data/pmfail.piz" "8:17: ⦠ Failed to match ‘{ρ₁ `just}’ against ‘{a `just ⊕ `nil}’"
-      ]
-
-rFile :: [FilePath] -> FilePath -> TestTree
-rFile incls fp = testCase fp $ rr incls fp
+      ( tErr ["."] "test/data/pmfail.piz" "8:17: ⦠ Failed to match ‘{ρ₁ `just}’ against ‘{a `just ⊕ `nil}’"
+      : [ tI fp | fp <- [ "lib/list.piz"
+                        , "prelude/ord.piz"
+                        , "test/examples/bool.piz"
+                        , "test/examples/mutual.piz"
+                        , "test/examples/maybe.piz"
+                        , "test/data/beta.piz"
+                        , "test/examples/ifte.piz"
+                        , "test/examples/pat.piz"
+                        ] ]
+      ++ [ tNo fp | fp <- ["examples/vierergruppe.piz", "prelude/fn.piz" ] ])
+    where tI = tFile ["."]; tNo = tFile []
 
 tErr :: [FilePath] -> FilePath -> String -> TestTree
 tErr incls fp expected = testCase fp $ do
@@ -34,13 +32,6 @@ tErr incls fp expected = testCase fp $ do
 tFile :: [FilePath] -> FilePath -> TestTree
 tFile incls fp = testCase fp $ do
     res <- tMs incls fp
-    case res of
-        Right{} -> pure ()
-        Left e  -> assertFailure (show e)
-
-rr :: [FilePath] -> FilePath -> Assertion
-rr incls fp = do
-    res <- rMs incls fp
     case res of
         Right{} -> pure ()
         Left e  -> assertFailure (show e)
