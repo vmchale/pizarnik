e8f231f4cb5369759e5f43334548fa35b0551499
Vanessa McHale <vamchale@gmail.com>
2025-03-16 18:47:47 -0400

    weed

--- src/Pr.hs
+++ src/Pr.hs
@@ -1,21 +1,18 @@
 module Pr ( pBound, pB
           , (<#>)
           , (<##>)
-          , (<#?>)
           , sq
           ) where
 
 import qualified Data.IntMap   as IM
-import           Prettyprinter (Doc, Pretty (pretty), hardline, line, vsep, (<+>))
+import           Prettyprinter (Doc, Pretty (pretty), hardline, vsep, (<+>))
 
 infixr 6 <#>
 infixr 6 <##>
-infixr 6 <#?>
 
-(<#>), (<##>), (<#?>) :: Doc ann -> Doc ann -> Doc ann
+(<#>), (<##>) :: Doc ann -> Doc ann -> Doc ann
 x <#> y = x <> hardline <> y
 x <##> y = x <> hardline <> hardline <> y
-x <#?> y = x <> line <> y
 
 pB :: (Pretty c, Pretty b) => (c, b) -> Doc a
 pB (i,j) = pretty i <+> "→" <+> pretty j
