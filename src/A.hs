3723a5cfb4233945113750cf14436a730715dc6a
Vanessa McHale <vamchale@gmail.com>
2025-03-24 14:23:51 -0400

    fmt

--- src/A.hs
+++ src/A.hs
@@ -160,7 +160,7 @@ instance Pretty (T a) where
     pretty (RV _ n s) = parens (pretty n <+> "⊃" <+> brsep ", " (pretty<$>S.toList s)) where brsep = encloseSep "{" "}"
     pretty (UU _ t) = concatWith (\x y -> x <+> "∪" <+> y) (pretty<$>t)
 
-pΣ = group.align.braces.fillSep.punctuate (flatAlt " ⊕ " " ⊕")
+pΣ = group.align.braces.fillSep.punctuate (flatAlt " ⊕" " ⊕")
 
 instance Show (T a) where show=show.pretty
 
