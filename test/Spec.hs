ef8015c9e8ade1f7f7845c1f54805a68c5353384
Vanessa McHale <vamchale@gmail.com>
2025-03-25 10:40:19 -0400

    shufl

--- test/Spec.hs
+++ test/Spec.hs
@@ -18,6 +18,7 @@ main = defaultMain $
                         , "test/examples/pat.piz"
                         , "test/data/beta.piz"
                         , "test/examples/exp.piz"
+                        , "test/data/permeable.piz"
                         ] ]
       ++ [ tNo fp | fp <- ["examples/vierergruppe.piz", "prelude/fn.piz" ] ])
     where tI = tFile ["."]; tNo = tFile []
