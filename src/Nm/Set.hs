b353ffd5d3b6fe5d2fb227503afd472034ddb387
Vanessa McHale <vamchale@gmail.com>
2025-04-03 00:19:40 -0400

    oops

---
+++ src/Nm/Set.hs
@@ -0,0 +1,11 @@
+module Nm.Set ( singleton
+              , insert
+              , member
+              ) where
+
+import qualified Data.IntSet as IS
+import           Nm
+
+singleton (Nm _ (U i) _) = IS.singleton i
+insert (Nm _ (U i) _) = IS.insert i
+member (Nm _ (U i) _) = IS.member i
