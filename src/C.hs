ecacd66caffac9c9f7ea50e44e5dd036e0f1cf7e
Vanessa McHale <vamchale@gmail.com>
2025-10-23 09:38:43 -0400

    viel besser

--- src/C.hs
+++ src/C.hs
@@ -1,13 +1,11 @@
 module C ( pᵤ ) where
 
 import           Data.Function (fix)
-import           Data.String   (IsString)
+import qualified Data.Text     as T
 
-pᵤ :: (Integral a, IsString b, Semigroup b) => a -> b
+pᵤ :: Int -> T.Text
 pᵤ i = if i<0 then "₋" <> p (-i) else p i
   where
     p = fix (\r d -> let (q,s) = d `quotRem` 10 in if q==0 then g s else r q<>g s)
       where
-        g 0="₀"; g 1="₁"; g 2="₂"; g 3="₃"; g 4="₄";
-        g 5="₅"; g 6="₆"; g 7="₇"; g 8="₈"; g 9="₉"
-        g _=error "?"
+        g c = T.singleton (toEnum (c+8320))
