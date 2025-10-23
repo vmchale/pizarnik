c3427b3bd6008ca5f2b64689823a8cc2e514bb3b
Vanessa McHale <vamchale@gmail.com>
2025-10-23 01:36:00 -0400

    use

--- src/G.hs
+++ src/G.hs
@@ -2,7 +2,7 @@ module G ( Sn, gp, gc ) where
 
 import qualified Data.Array         as A
 import qualified Data.Array.Unboxed as UA
-import           Data.Bits          (Bits (shiftL, (.&.), (.|.)))
+import           Data.Bits          (Bits (setBit, testBit))
 
 -- TODO: permutation itself could be a Word (4-bit integers lol)
 
@@ -19,12 +19,12 @@ gc p = step 0 1
                 Just j' -> let (v',cyc) = orb v j' in cyc:step v' j'
           where
             next i | i==n = Nothing
-                   | v .&. (1 `shiftL` i) /= 0 = next (i+1)
+                   | testBit v i = next (i+1)
                    | otherwise = Just i
 
         orb :: Word -> Int -> (Word, [Int])
-        orb v j | v .&. (1 `shiftL` j) /= 0 = (v, [])
-                | otherwise = let k=p UA.! j; (r, c) = orb (v .|. (1 `shiftL` j)) k in (r, j:c)
+        orb v j | testBit v j = (v, [])
+                | otherwise = let k=p UA.! j; (r, c) = orb (setBit v j) k in (r, j:c)
 
 gp :: Sn -> [a] -> [a]
 gp p xs = A.elems (A.array (UA.bounds p) (zip (UA.elems p) xs))
