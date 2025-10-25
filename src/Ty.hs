4a17af5d31f0bf686bb85357dd095fdc3b770161
Vanessa McHale <vamchale@gmail.com>
2025-10-25 01:15:47 -0400

    assume local lookups more common... hm

--- src/Ty.hs
+++ src/Ty.hs
@@ -464,7 +464,6 @@ lt c t0 (TC _ n) = do {t1 <- lC (tβ c) n; lt c t0 t1}
 -- TODO: pass cs<>c to lt directly and then use lΒ instead of βc
 lt c t0 t1 | Just{} <- unA t0 = do {t0' <- βc (tβ c) t0; lt c t0' t1}
 lt c t0 t1 | Just{} <- unA t1 = do {t1' <- βc (tβ c) t1; lt c t0 t1'}
--- TODO: this would fail for TC like type H=Int (0-ary type synonyms)
 lt c t0@(Ρ _ n σ0) t1@(Σ _ σ1)
     | occρ n σ1 = throwError$O t0 t1
     | σ0 `Nm.isSubmapOf` σ1 = iTV n t1 <$> mσ lt c σ0 σ1
@@ -483,8 +482,8 @@ lt c t0@(Ρ _ n0 σ0) t1@(Ρ _ n1 σ1) | occρ n0 σ1 = throwError$O t0 t1
                                    | otherwise = sf t0 t1
 lt _ t0@(TP _ l0) t1@(TP _ l1) | l0==l1 = pure mempty
                                | otherwise = sf t0 t1
-lt c t0@UU{} t1 = do {t0' <- um (tβ c) t0; lt c t0' t1}
-lt c t0 t1@UU{} = do {t1' <- um (tβ c) t1; lt c t0 t1'}
+lt c t0@UU{} t1 = do {t0' <- uU (tβ c) t0; lt c t0' t1}
+lt c t0 t1@UU{} = do {t1' <- uU (tβ c) t1; lt c t0 t1'}
 lt _ t0@TP{} t1@QT{} = sf t0 t1; lt _ t0@QT{} t1@TP{} = sf t0 t1
 lt _ t0@TP{} t1@TT{} = sf t0 t1; lt _ t0@TT{} t1@TP{} = sf t0 t1
 lt _ t0@TT{} t1@QT{} = sf t0 t1; lt _ t0@QT{} t1@TT{} = sf t0 t1
@@ -492,27 +491,26 @@ lt _ t0@TP{} t1@Σ{} = sf t0 t1; lt _ t0@Σ{} t1@TP{} = sf t0 t1
 lt _ t0@QT{} t1@Σ{} = sf t0 t1; lt _ t0@Σ{} t1@QT{} = sf t0 t1
 lt _ SV{} _ = ie; lt _ _ SV{} = ie
 
-um :: Cs a -> T a -> TM a (T a)
-um c t = do {c' <- gets (tds.lo); uU (c<>c') t}
-  where
-    uU :: Cs a -> T a -> TM a (T a)
-    uU c te@(UU x ts) = Σ x <$> foldMapM f ts where
-        f (TT _ n)   = pure (Nm.singleton n [])
-        f (Σ _ σ)    = pure σ
-        f t          | Just{} <- unA t = f =<< lΒ c t
-        f (TC _ n)   = f (lC₁ c n)
-        f (UU _ ts_) = foldMapM f ts_
-        -- TODO: unions on variables? (could end up being instantiated wrong idk if that's useful tho)
-        f SV{}       = ie
-        f Ρ{}        = ie
-        f TP{}       = throwError$Bare te
-        f QT{}       = throwError$Bare te
+uU :: Cs a -> T a -> TM a (T a)
+uU c te@(UU x ts) = Σ x <$> foldMapM f ts where
+    f (TT _ n)   = pure (Nm.singleton n [])
+    f (Σ _ σ)    = pure σ
+    f t          | Just{} <- unA t = f =<< βc c t
+    f (TC _ n)   = f =<< lC c n
+    f (UU _ ts_) = foldMapM f ts_
+    -- TODO: unions on variables? (could end up being instantiated wrong idk if that's useful tho)
+    f SV{}       = ie
+    f Ρ{}        = ie
+    f TP{}       = throwError$Bare te
+    f QT{}       = throwError$Bare te
 
 lC :: Cs a -> Nm a -> TM a (T a)
-lC c n = do {cs <- gets (tds.lo); pure$lC₁ (c<>cs) n}
-
-lC₁ :: Cs a -> Nm a -> T a
-lC₁ c (Nm _ (U i) _) | Just ([],t) <- IM.lookup i c = t
+lC ex (Nm _ (U i) _) = do
+    cs <- gets (tds.lo)
+    case IM.lookup i cs of
+        Just ([],t) -> pure t
+        Nothing -> case IM.lookup i ex of
+            Just ([],t) -> pure t
 
 -- TODO: expand UU...
 βc c t = do {cs <- gets (tds.lo); lΒ (c<>cs) t}
@@ -729,17 +727,20 @@ uz = foldr (\(TS l r) ~(ls,rs) -> (l:ls, r:rs)) ([], [])
 dU :: Nt a -> Subst a -> a -> [TS a] -> TM a (TS a, Subst a)
 dU c s x tss = do
     ls <- traverse (βs c) l0; rs <- traverse (βs c) r0
-    -- TODO: padding should be entwined with un-picking the
     let rm=maximum (length<$>rs)
     ρ <- zipWithM pad (tLs<$>l0) [ rm-length r | r <- rs ]
     let ls'=zipWith tuck ρ ls; rs'=zipWith tuck ρ rs
     tψ <- ψ c (zt ls' rs')
+    -- (al,s') <- traceShow tψ $ srs s (Nm.toList x tψ)
     (al,s') <- srs s (Nm.toList x tψ)
     (σ,ul) <- an (map (second tlefts) al)
     (l',s'') <- urs s' ul; (r',s''') <- frs s'' rs'
     pure (l'++[σ] --: r', s''')
   where (l0,r0) = uz tss
-        rr=ars c
+        -- TODO: lT rr is constant, why not sum
+        -- local <- gets (arit.lo)
+        -- and pass (ars c<>local<>ars c)
+        lR=lT (ars c)
 
         tuck ts0 (t@SV{}:ts1) = t:ts0++ts1
 
@@ -756,7 +757,7 @@ dU c s x tss = do
         urs sϵ (t:ts) = do {(tr,s0) <- urs sϵ ts; usc c s0 tr t}
 
         an as = do
-            (tas, tls) <- unzip<$>traverse (\(nm,ts) -> do{n<-lT rr nm; when (n>length ts) (error"Internal error?") $> (ts /| n)}) as
+            (tas, tls) <- unzip<$>traverse (\(nm,ts) -> do{n<-lR nm; when (n>length ts) (error"Internal error?") $> (ts /| n)}) as
             pure (Σ x (Nm.fromDistinctAscList (zip nms tls)), tas)
           where nms=map fst as
 
