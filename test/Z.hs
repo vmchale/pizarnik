91332557f1897738951a9936af125177fde847e4
Vanessa McHale <vamchale@gmail.com>
2025-11-23 09:49:37 -0500

    simplify

--- test/Z.hs
+++ test/Z.hs
@@ -10,28 +10,28 @@ main :: IO ()
 main = defaultMain $
     testGroup "unit"
         [ testGroup "e"
-          [ eEx ["."] "test/data/list.piz" "six" "6"
-          , eEx ["."] "lib/maybe.piz" "`nothing join" "`nothing"
-          , eEx [] "examples/vierergruppe.piz" "`b `c mult" "`a"
-          , eEx [] "prelude/bool.piz" "True False or" "True"
-          , eEx [] "test/examples/mutual.piz" "5 even" "False"
-          , eEx ["."] "lib/numbertheory.piz" "15 10 gcd" "5"
-          , eEx ["."] "prelude/ord.piz" "3 2 cmpInt" "`gt"
-          , eEx [] "test/examples/ros.piz" "`g complement" "`c"
+          [ eEx "test/data/list.piz" "six" "6"
+          , eEx "lib/maybe.piz" "`nothing join" "`nothing"
+          , eEx "examples/vierergruppe.piz" "`b `c mult" "`a"
+          , eEx "prelude/bool.piz" "True False or" "True"
+          , eEx "test/examples/mutual.piz" "5 even" "False"
+          , eEx "lib/numbertheory.piz" "15 10 gcd" "5"
+          , eEx "prelude/ord.piz" "3 2 cmpInt 2 2 cmpInt" "`gt `eq"
+          , eEx "test/examples/ros.piz" "`g complement `t complement" "`c `a"
+          , eEx "test/examples/parity.piz" "`even `even add `odd `odd add" "`even `even"
           ]
         , testGroup "ty"
-              -- TODO: error line no.?
-            ( tENo "test/data/pmfail.piz" "3:12: {a `just ⊕ `nil} ⊀ {a `just}"
-            : tENo "test/data/badPerm.piz" "?"
-            : tENo "test/data/univL.piz" "1:5: could not match ‘Int’ against ‘a’"
-            : tENo "test/data/univR.piz" "1:8: could not match ‘Int’ against ‘a’"
-            : tENo "test/examples/errorHierarchy.piz" "?"
-            : tENo "test/data/badBool.piz" "3:12: failed to unify ‘{True}’ with ‘{True ⊕ False}’"
-            : tENo "test/data/badBool2.piz" "3:12: failed to unify ‘{False}’ with ‘{True ⊕ False}’"
+            ( tE "test/data/pmfail.piz" "3:12: {a `just ⊕ `nil} ⊀ {a `just}"
+            : tE "test/data/badPerm.piz" "?"
+            : tE "test/data/univL.piz" "1:5: could not match ‘Int’ against ‘a’"
+            : tE "test/data/univR.piz" "1:8: could not match ‘Int’ against ‘a’"
+            : tE "test/examples/errorHierarchy.piz" "?"
+            : tE "test/data/badBool.piz" "3:12: failed to unify ‘{True}’ with ‘{True ⊕ False}’"
+            : tE "test/data/badBool2.piz" "3:12: failed to unify ‘{False}’ with ‘{True ⊕ False}’"
             : tE "test/data/permeable.piz" "21:8: ‘{`nil ⊕\n       List( Unit ) Unit `cons}’ is not an acceptable argument, expected ‘{List( a ) a `cons}’"
             : tE "test/data/badList.piz" "4:14: {List(a) a `cons ⊕ `nil} ⊀ {ρ₁ a `cons}"
             : tE "test/data/both.piz" "12:5: {{True ⊕ False} `left ⊕ Int `right ⊕ {True ⊕ False} Int `both} ⊀ {{True ⊕\n                                                                        False} `left ⊕\n                                                                       Int `right}"
-            : [ tI fp | fp <- [ "lib/list.piz"
+            : [ tF fp | fp <- [ "lib/list.piz"
                               , "lib/either.piz"
                               , "lib/both.piz"
                               , "test/examples/maybe.piz"
@@ -40,30 +40,27 @@ main = defaultMain $
                               , "test/data/beta.piz"
                               , "test/data/rec.piz"
                               , "test/examples/pat2.piz"
-                              ] ]
-            ++ [ tNo fp | fp <- [ "test/examples/klein.piz"
-                                , "test/data/perm.piz"
-                                , "test/examples/exp.piz"
-                                , "prelude/fn.piz"
-                                ] ])
+                              , "test/examples/klein.piz"
+                              , "test/data/perm.piz"
+                              , "test/examples/exp.piz"
+                              , "prelude/fn.piz"
+                              ] ])
         ]
-    where tI = tFile ["."]; tNo = tFile []
-          tE = tErr ["."]; tENo = tErr []
 
-eEx :: [FilePath] -> FilePath -> BSL.ByteString -> String -> TestTree
-eEx incls fp src expected = testCase (ASCIIL.unpack src ++ " (" ++ fp ++ ")") $
-    e1 incls [fp] src >>= \case
+eEx :: FilePath -> BSL.ByteString -> String -> TestTree
+eEx fp src expected = testCase (ASCIIL.unpack src ++ " (" ++ fp ++ ")") $
+    e1 ["."] [fp] src >>= \case
         Left e -> assertFailure (show e)
-        Right [e] -> show e @?= expected
+        Right e -> unwords (map show (reverse e)) @?= expected
 
-tErr :: [FilePath] -> FilePath -> String -> TestTree
-tErr incls fp expected = testCase fp $
-    rRepl (tMs incls [fp]) >>= \case
+tE :: FilePath -> String -> TestTree
+tE fp expected = testCase fp $
+    rRepl (tMs ["."] [fp]) >>= \case
         Right{} -> assertFailure "expected error."
         Left e  -> show e @?= expected
 
-tFile :: [FilePath] -> FilePath -> TestTree
-tFile incls fp = testCase fp $ do
-    rRepl (tMs incls [fp]) >>= \case
+tF :: FilePath -> TestTree
+tF fp = testCase fp $ do
+    rRepl (tMs ["."] [fp]) >>= \case
         Right{} -> pure ()
         Left e  -> assertFailure (show e)
