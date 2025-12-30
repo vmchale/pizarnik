b23e989f2d2e440d23bb3917c576f45e188e8287
Vanessa McHale <vamchale@gmail.com>
2025-12-30 10:24:32 -0500

    remove Ord instance for Nm (unnecessary?)

--- src/Nm.hs
+++ src/Nm.hs
@@ -20,7 +20,6 @@ instance Eq MN where (==) (MN _ u) (MN _ u') = u == u'
 instance Ord MN where compare (MN _ u) (MN _ u') = compare u u'
 
 instance Eq (Nm a) where (==) (Nm _ u _) (Nm _ u' _) = u == u'
-instance Ord (Nm a) where compare (Nm _ u _) (Nm _ u' _) = compare u u'
 
 instance Pretty (Nm a) where
     pretty (Nm t _ _) = pretty t
