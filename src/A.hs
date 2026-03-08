4e3dd649db2f3c607cc695390c9a2d9ad611c505
Vanessa McHale <vamchale@gmail.com>
2026-03-08 16:32:12 -0400

    I think this is better?

--- src/A.hs
+++ src/A.hs
@@ -233,7 +233,7 @@ instance Pretty (A a) where
     pretty (C _ n) = pretty n; pretty (V _ n) = pretty n; pretty (Inv _ a) = pretty a <> "⁻¹"
     pretty (Ca _ as) = braces (hsep (pretty<$>reverse as))
 
-pA = concatWith (\x y -> x <+> "&" <> line <> y)
+pA = concatWith (\x y -> x <#> "&" <+> y)
 
 pSeq :: P0 a => [a] -> Doc ann
 pSeq = hsep.map p0
