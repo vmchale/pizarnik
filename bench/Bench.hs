e1dee25042aab2cf679d46813674d23e150687a7
Vanessa McHale <vamchale@gmail.com>
2026-03-09 13:04:31 -0400

    big

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -6,8 +6,12 @@ import           P
 main = defaultMain
     [ bgroup "t"
         [ b fp | fp <- [ "test/examples/maybe.piz"
-                       , "lib/list.piz"
+                       , "lib/fingertree.piz"
                        , "examples/vierergruppe.piz"
                        ]
         ]
+    , bgroup "e"
+        [ be ["examples/set.piz", "test/examples/set.piz"] "x 3 x member 6 x member 7 x member"
+        ]
     ] where b fp = bench fp $ whnfIO $ rRepl $ tMs ["."] [fp]
+            be fp x = bench (head fp) $ whnfIO $ e1 ["."] fp x
