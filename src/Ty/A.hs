cf9ac9d9d831806ac36823d1d337fd8e10c2c657
Vanessa McHale <vamchale@gmail.com>
2026-07-12 09:56:37 -0400

    hm

--- src/Ty/A.hs
+++ src/Ty/A.hs
@@ -47,4 +47,5 @@ cloneSigM (TS tl tr) = TS <$> traverse cT tl <*> traverse cT tr
     cT (QT x ts) = QT x <$> cloneSigM ts
     cT (TA x t ts) = TA x <$> cT t <*> cT ts
     cT (Σ x ts) = Σ x <$> traverse (traverse cT) ts
+    cT (UU x ts) = UU x <$> traverse cT ts
     cT (Ρ x n a) = Ρ x <$> tryTV n <*> traverse (traverse cT) a
