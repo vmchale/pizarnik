be15903b55ce894790d5fe585bff5f433d996593
Vanessa McHale <vamchale@gmail.com>
2026-08-24 13:24:11 -0400

    b

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -13,6 +13,7 @@ main = defaultMain
         ]
     , bgroup "e"
         [ be ["examples/set.piz", "test/examples/set.piz"] "x 3 x member 6 x member 7 x member"
+        , be ["examples/ast.piz"] "0 \"b\" `name `var 0 \"a\" `name `var `ap 0 \"a\""
         ]
     ] where b fp = bench fp $ whnfIO $ rRepl $ tMs ["."] [fp]
             be fp x = bench (head fp) $ whnfIO $ e1 ["."] fp x
