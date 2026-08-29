1b839a8edebe0142aaa9371b0f6b58f2b0dae012
Vanessa McHale <vamchale@gmail.com>
2026-08-29 11:22:34 -0400

    reconcile reversion of "maybe this is unnecessary hm"
    This reverts commit 9e5114ff45a6d30b173d246eac0bc15d25832f2d.

--- src/P.hs
+++ src/P.hs
@@ -59,10 +59,10 @@ naïve c = (foldMap ((lm.snd4)@<>) c, foldMap (thd4@<>) c, IM.fromDistinctAscLis
 
 rc :: Int -> MC (TS Loc) Loc -> S Loc -> ASeq Loc -> Either (E Loc) (S Loc, Int)
 rc i c s at = flip runStateT i $ do
-    (t,a) <- μ $ tAS e s at
+    (t,a) <- mapStateT (first TyE) $ tAS e s at
     case t of
       (TS (_:_:_) _) -> throwError ES
-      _              -> μ $ rty e (aLs at) =<< r c (aas a) s
+      _              -> μ.rty e (aLs at) $ r c (aas a) s
   where
     e = let (b,cϵ,a)=c in Ext (fmap aLs b) cϵ a
     μ = mapStateT (first TyE)
--- src/S.hs
+++ src/S.hs
@@ -11,15 +11,15 @@ import           Nm
 import qualified Nm.Map        as Nm
 import           Pr
 import           Prettyprinter (Doc, pretty)
-import           Ty            (UM)
 
 type S a = [A (TS a)]
 
 type F a = IM.IntMap (ASeq a)
 type MC a b = (F a, Cs b, Ar)
 
-r :: MC (TS a) a -> [A (TS a)] -> S a -> UM a (S a)
-r e as v = s (pure v) as where s c [] = c; s c (a:aa) = s (ι e a =<< c) aa
+r :: MC (TS a) a -> [A (TS a)] -> S a -> S a
+r e as v = s v as where s c [] = c; s c (a:aa) = s (ι e a c) aa
+-- r e as = thread (map (ι e) (reverse as)) where thread = foldr (.) id
 
 lm :: M b a -> F a
 lm (M _ ds) = f ds where f [] = IM.empty; f (d:dd) = b d (f dd)
@@ -27,18 +27,24 @@ lm (M _ ds) = f ds where f [] = IM.empty; f (d:dd) = b d (f dd)
 b :: D b a -> F a -> F a
 b (F _ (Nm _ (U i) _) _ as) = IM.insert i as; b TD{} = id
 
-i_ c a = (\case [L t (I i)] -> (i,t)) <$> ι c a []; s_ c a = (\case [L t (Str s)] -> (s,t)) <$> ι c a []
+i_ c a | [L t (I i)] <- ι c a [] = (i,t); s_ c a | [L t (Str s)] <- ι c a [] = (s,t)
 
 ta l=let nm=true l;t=TS [] [TT l nm] in C t (nm$>t)
 fa l=let nm=false l;t=TS [] [TT l nm] in C t (nm$>t)
 
-i2 c op (a0:a1:as) = do (i0,_) <- i_ c a0; (i1,t) <- i_ c a1; pure$L t (I$i1`op`i0):as
-ib c rel (a0:a1:as) = do (i0,_) <- i_ c a0; (i1,TS _ rs) <- i_ c a1; pure$bt (tL$head rs) (i1`rel`i0):as
+-- i2 c op (a0:a1:as) = do (i0,_) <- i_ c a0; (i1,t) <- i_ c a1; pure$L t (I$i1`op`i0):as
+-- ib c rel (a0:a1:as) = do (i0,_) <- i_ c a0; (i1,TS _ rs) <- i_ c a1; pure$bt (tL$head rs) (i1`rel`i0):as
+    -- where bt l True  = ta l
+          -- bt l False = fa l
+
+i2 c op (a0:a1:as) = let (i0,_)=i_ c a0;(i1,t)=i_ c a1 in L t (I$i1`op`i0):as
+ib c rel (a0:a1:as) = let (i0,_)=i_ c a0;(i1,TS _ rs)=i_ c a1 in bt (tL$head rs) (i1`rel`i0):as
     where bt l True  = ta l
           bt l False = fa l
 
-ψ :: MC (TS a) a -> [ASeq (TS a)] -> S a -> UM a (S a)
+ψ :: MC (TS a) a -> [ASeq (TS a)] -> S a -> S a
 ψ c@(_,cϵ,_) aa (k:as) | t <- last (trights (aL k)), Just as₀ <- find (h t) (map aas aa) = r c as₀ (k:as)
+-- ψ c@(_,cϵ,_) aa (k:as) | t <- last (trights (aL k)), Just as₀ <- find (h t) (map aas aa) = r c (tail as₀) (u k as)
   where
     h t (a:_) | t' <- last (tlefts (aL a)), t ≺ t' = True
               | otherwise = False
@@ -49,9 +55,10 @@ ib c rel (a0:a1:as) = do (i0,_) <- i_ c a0; (i1,TS _ rs) <- i_ c a1; pure$bt (tL
     t₀ ≺ t₁ | Just (TC _ n, s) <- tun t₁, Right t₁' <- β cϵ n s = t₀ ≺ t₁'
     _ ≺ _                   = False
 
-ι :: MC (TS a) a -> A (TS a) -> S a -> UM a (S a)
-ι _ (B _ Dup) (a:as)       = pure (a:a:as)
-ι _ (B _ Un) (_:as)        = pure as
+
+ι :: MC (TS a) a -> A (TS a) -> S a -> S a
+ι _ (B _ Dup) (a:as)       = a:a:as
+ι _ (B _ Un) (_:as)        = as
 ι c (B _ Plus) as          = i2 c (+) as
 ι c (B _ Minus) as         = i2 c (-) as
 ι c (B _ Mul) as           = i2 c (*) as
@@ -60,23 +67,19 @@ ib c rel (a0:a1:as) = do (i0,_) <- i_ c a0; (i1,TS _ rs) <- i_ c a1; pure$bt (tL
 ι c (B _ Eq) as            = ib c (==) as
 ι c (B _ Gt) as            = ib c (>) as
 ι c (B _ Lt) as            = ib c (<) as
-ι c (B _ Cat) (a0:a1:as)   = do (s0,_) <- s_ c a0; (s1,t) <- s_ c a1; pure$L t (Str$s1<>s0):as
+ι c (B _ Cat) (a0:a1:as)   = let (s0,_)=s_ c a0; (s1,t)=s_ c a1 in L t (Str$s1<>s0):as
 ι c (B _ Ap) (Q _ a:as)    = r c (aas a) as
-ι c (B _ Dip) (Q _ f:a:as) = (a:) <$> r c (aas f) as
-ι _ (L _ (S p)) a          = let n = gn p; (x,a_)=splitAt n a in pure (gp p x++a_)
-ι _ a@L{} as               = pure (a:as)
-ι _ a@Q{} as               = pure (a:as)
+ι c (B _ Dip) (Q _ f:a:as) = a:r c (aas f) as
+ι _ (L _ (S p)) a          = let n = gn p; (x,a_)=splitAt n a in gp p x++a_
+ι _ a@L{} as               = a:as
+ι _ a@Q{} as               = a:as
 -- FIXME: type catenation? FIXME don't pinch off if there aren't enough...
-ι c a@(C (TS _ tr) tt) as  =
-    let n=lA c tt; (x,a_)=splitAt n as
-        (ᴀ:_)=tr
-    in pure (if n==0 then a:as else Ca (TS [ᴀ] tr) (a:x):a_)
-ι c (Pat _ (SL _ aa)) as   = ψ c aa as -- FIXME: this pinches off stack variables...?
+ι c a@(C (TS _ tr) tt) as  = let n=lA c tt; (x,a_)=splitAt n as; (ᴀ:_)=tr in if n==0 then a:as else let in Ca (TS [ᴀ] tr) (a:x):a_
+ι c (Pat _ (SL _ aa)) as   = ψ c aa as -- FIXME: this pinches off stack variables...
 ι c (V _ n) as             = let a = lV c n in r c (aas a) as
-ι _ (Inv _ (C _ tt₀)) (Ca _ (C _ tt₁:cs):as) | tt₀==tt₁ = pure (cs++as)
-ι _ (Inv _ (C _ tt₀)) (C _ tt₁:as) | tt₀==tt₁ = pure as
+ι _ (Inv _ (C _ tt₀)) (Ca _ (C _ tt₁:cs):as) | tt₀==tt₁ = cs++as
+ι _ (Inv _ (C _ tt₀)) (C _ tt₁:as) | tt₀==tt₁ = as
 ι c a₀@Inv{} (a₁@Inv{}:as) = r c [a₀,a₁] as
-ι _ a₀ aa = error (show (a₀,aa))
 
 lA :: MC a b -> Nm a -> Int
 lA (_,_,a) (Nm _ (U u) _) | Just ar <- a IM.!? u = ar
